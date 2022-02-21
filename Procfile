27cf7b0aaa1f87fefb234ae46825debe188d8e90
Matias Maretto <marettome@globalhitss.com>
2022-02-21 01:18:27 -0300

    First commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorni index:app
---
+++ index.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, send_file
+
+app = Flask(__name__)
+
+@app.route( '/' )
+def index():
+    return render_template( 'index.html' )
+
+@app.route( '/about' )
+def experience():
+    return render_template( 'experience.html' )
+
+def image():
+    return send_file('templates/images/profile_foto_rounded.png',  mimetype='image/png')
+
+if __name__ == '__main__':
+    app.run( debug=True )
