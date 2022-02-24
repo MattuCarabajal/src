8da7807d8c097db547917acc63b3ec68e90dd476
Matias Maretto <marettome@globalhitss.com>
2022-02-24 19:11:33 -0300

    index.html-Updated presentation as interface-Deleted both navbars-New buttons to navegate

--- index.py
+++ index.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def index():
     return render_template( 'index.html' )
 
-@app.route( '/about' )
+@app.route( '/experience' )
 def experience():
     return render_template( 'experience.html' )
 
